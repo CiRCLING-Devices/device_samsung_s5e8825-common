07711a7dd72ab5731c7833b00702d977f48819c7
Andreas Schneider <asn@cryptomilk.org>
2020-03-11 20:37:29 +0100

    s5e8825-common: build lineage livedisplay HAL
    * Also set permissions for mdnie lux node in init.s5e8825.rc
    
    Change-Id: I11dfc1df9eb4bd30c311d7b65ae6b4d1c59f54cb
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- common.mk
+++ common.mk
@@ -187,6 +187,9 @@ $(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/af
 $(call soong_config_set,lineage_health,fast_charge_value_none,1)
 $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
+# Live Display
+PRODUCT_PACKAGES += vendor.lineage.livedisplay@2.0-service.samsung-exynos
+
 # Memtrack
 PRODUCT_PACKAGES += android.hardware.memtrack-service.samsung-mali
 
