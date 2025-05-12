a641ce895152ed10009a876ddbc39718ea5ac376
alternoegraha <noegrahachan@gmail.com>
2025-05-12 18:59:02 +0700

    s5e8825-common: Drop `TARGET_CPU_ABI2`
    * We moved on to 64 bit only
    
    Change-Id: I109d2a534ce417b5d127d609f9ba58041abf771f
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,7 +22,6 @@ include vendor/samsung/s5e8825-common/BoardConfigVendor.mk
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 
 # ART
