29a59115e146f17eec575ab0480088b1d47eef82
Flopster101 <nahuelgomez329@gmail.com>
2025-06-03 20:11:09 -0300

    s5e8825-common: Inherit private keys
    Change-Id: I98e50581bd219d51d9eb484f1d0d85c0ba096578
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -19,6 +19,9 @@ $(call inherit-product, vendor/samsung/s5e8825-common/s5e8825-common-vendor.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Private keys
+$(call inherit-product-if-exists, vendor/lineage-priv/keys/keys.mk)
+
 # Graphics
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := 450dpi
