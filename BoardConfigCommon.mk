c97b00ddd53b8e65f7b712feb5412c7034f2115a
Majaahh <maja.blaszczak@proton.me>
2025-08-28 12:44:44 +0200

    s5e8825-common: Include common exynos sepolicy
    Change-Id: Id4819c9e77b6486f7d1a95ec22b287bbceba99c0
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,6 +84,7 @@ TARGET_SOC := s5e8825
 
 # SELinux
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
+include device/lineage/sepolicy/exynos/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
 # Verified Boot
