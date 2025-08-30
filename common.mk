a3a5b6ce7abad399c728273afb232eb9d95c54b6
Majaahh <maja.blaszczak@proton.me>
2025-08-30 14:12:27 +0200

    s5e8825-common: Build custom Samsung DAP
    Change-Id: I39065a7aae9bcdd0ac32917c4c317b968c218256
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -168,6 +168,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/linker/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Samsung DAP
+PRODUCT_PACKAGES += \
+    SamsungDAP-custom
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
