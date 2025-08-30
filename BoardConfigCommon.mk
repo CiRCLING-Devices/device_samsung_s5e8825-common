59d323d1fd87f80a8576b87cd35fd6c96e7ceb4c
Tim Zimmerman <tim@linux4.de>
2025-08-30 14:10:34 +0200

    s5e8825-common: Setup Bluetooth
    Change-Id: I76f71bce0fefce0033e4275a10730ca1864c89ac
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,6 +32,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 
+# Bluetooth
+BOARD_HAVE_BLUETOOTH_SLSI := true
+
 # DTB
 BOARD_DTB_CFG := $(COMMON_PATH)/configs/kernel/s5e8825.cfg
 
--- common.mk
+++ common.mk
@@ -49,6 +49,16 @@ PRODUCT_COPY_FILES += \
 
 TARGET_EXCLUDES_AUDIOFX := true
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl:64 \
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor:64
+
+PRODUCT_COPY_FILES += \
+    hardware/samsung_slsi/libbt/conf/bt_did.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_did.conf \
+    hardware/samsung_slsi/libbt/conf/bt_vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images_vendor
