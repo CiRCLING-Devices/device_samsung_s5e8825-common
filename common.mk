5a15b1a7ef50cbd13d52c3465ba406b43d6ada25
Tim Zimmerman <tim@linux4.de>
2025-08-30 20:21:24 +0200

    s5e8825-common: Add powerhint for Pixel Power HAL
    Change-Id: Ie65fd848d9d96e2d8b6feff5852e02c21a29111a
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -197,6 +197,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/linker/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
