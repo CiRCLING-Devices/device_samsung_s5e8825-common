cb9d3663f5bdefefb41464874fbf6b06d33c8d6b
Flopster101 <nahuelgomez329@gmail.com>
2025-08-30 13:06:28 +0200

    s5e8825-common: Setup Audio
    Change-Id: I112b505fbea88b3f7f6dc41efa6fd3a621600252
    Co-authored-by: Tim Zimmerman <tim@linux4.de>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -23,6 +23,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product-if-exists, vendor/lineage-priv/keys/keys.mk)
 
 # Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.service \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.bluetooth.audio-impl \
+    audio.bluetooth.default \
+    audio.primary.default \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(COMMON_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_configuration.xml \
+    frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_default_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_default_stream_volumes.xml \
+    frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_product_strategies.xml \
+    frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_stream_volumes.xml
+
 TARGET_EXCLUDES_AUDIOFX := true
 
 # ConfigStore
--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -12,6 +16,31 @@ from extract_utils.main import (
 module = ExtractUtilsModule(
     's5e8825-common',
     'samsung',
+)
+
+blob_fixups: blob_fixups_user_type = {
+   (
+        'vendor/lib/libaudioparamupdate.so',
+        'vendor/lib/libaboxpcmdump.so',
+        'vendor/lib/libaudioproxy2.so',
+        'vendor/lib64/libaudioparamupdate.so',
+        'vendor/lib64/libaboxpcmdump.so',
+        'vendor/lib64/libaudioproxy2.so',
+    ): blob_fixup()
+        .add_needed('libaudioroute.s5e8825.so')
+        .add_needed('libtinyalsa.s5e8825.so'),
+    (
+        'vendor/lib/hw/audio.primary.s5e8825.so',
+        'vendor/lib64/hw/audio.primary.s5e8825.so'
+    ): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute.s5e8825.so')
+        .replace_needed('libtinyalsa.so', 'libtinyalsa.s5e8825.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    's5e8825-common',
+    'samsung',
+    blob_fixups=blob_fixups,
 )
 
 if __name__ == '__main__':
