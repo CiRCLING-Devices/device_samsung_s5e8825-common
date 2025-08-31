9dcde66277695e0b43e6a02c3a5a4d29c6fd7ec5
Tim Zimmerman <tim@linux4.de>
2025-08-31 22:03:17 +0200

    s5e8825-common: Make libwvhidl use full libprotobuf
    Change-Id: I41cd538fbcdd97ba041bf7995266f9cdc89a7926
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -13,7 +13,9 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
     lib_fixups_user_type,
+    libs_proto_3_9_1,
 )
 
 namespace_imports = [
@@ -29,6 +31,7 @@ def lib_fixup_device_dep(lib: str, partition: str, *args, **kwargs):
 
 
 lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
     'libexynoscamera3': lib_fixup_device_dep,
 }  # fmt: skip
 
@@ -91,6 +94,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libsec-ril.so': blob_fixup()
         .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
             '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
