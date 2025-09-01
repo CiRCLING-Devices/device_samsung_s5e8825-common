e7631378ab8af7917af908ae08dc50ef3554e043
Majaahh <maja.blaszczak@proton.me>
2025-09-01 11:19:47 +0200

    fixup! s5e8825-common: Setup fingerprint
    Change-Id: Iaee707d527746a34cd7badfa5b32b247c29a7280
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -128,7 +128,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.s5e8825
+    android.hardware.biometrics.fingerprint-service.samsung-s5e8825
 
 ifeq ($(TARGET_HAS_UDFPS),true)
 PRODUCT_PACKAGES += \
