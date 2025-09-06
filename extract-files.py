5a5836dc1b2966cb1c2d2e0feb2978928ec0dafe
Majaahh <maja.blaszczak@proton.me>
2025-09-06 21:02:08 +0200

    s5e8825-common: Cleanup extract files
    Change-Id: I1a3fc405b18533d2cf428eabc0b1db60684d0355
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -37,30 +37,7 @@ lib_fixups: lib_fixups_user_type = {
 }  # fmt: skip
 
 blob_fixups: blob_fixups_user_type = {
-    'vendor/lib64/vendor.samsung.hardware.keymint-V1-ndk_platform.so': blob_fixup()
-        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so',
-            'android.hardware.security.keymint-V4-ndk.so')
-        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so',
-            'android.hardware.security.secureclock-V1-ndk.so')
-        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so',
-             'android.hardware.security.sharedsecret-V1-ndk.so')
-        .add_needed('android.hardware.security.rkp-V3-ndk.so')
-        .replace_needed('libcrypto.so', 'libcrypto-tm.so')
-        .replace_needed('libssl.so', 'libssl-tm.so')
-        .add_needed('libshim_crypto.so'),
-    'vendor/etc/init/android.hardware.security.keymint-service.samsung.rc': blob_fixup()
-        .regex_replace('android\\.hardware\\.security\\.keymint-service\n',
-            'android.hardware.security.keymint-service.samsung\n'),
-    'vendor/lib64/libsensorlistener.so': blob_fixup()
-        .add_needed('libshim_sensorndkbridge.so'),
-    (
-        'vendor/lib64/sensors.grip.so',
-        'vendor/lib64/sensors.inputvirtual.so',
-        'vendor/lib64/sensors.sensorhub.so',
-    ): blob_fixup()
-        .remove_needed('libhidltransport.so')
-        .add_needed('libutils-v32.so')
-        .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim'),
+    # Audio
     (
         'vendor/lib64/libaudioparamupdate.so',
         'vendor/lib64/libaboxpcmdump.so',
@@ -72,23 +49,53 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libaudioroute.so', 'libaudioroute.s5e8825.so')
         .replace_needed('libtinyalsa.so', 'libtinyalsa.s5e8825.so')
         .sig_replace('E1 00 00 54 A9 02 80 52 29 80 01 B9 08 65 40 B9',
-                    '1F 20 03 D5 A9 02 80 52 29 80 01 B9 08 65 40 B9')
+                     '1F 20 03 D5 A9 02 80 52 29 80 01 B9 08 65 40 B9')
         .sig_replace('E0 17 9F 1A FD 7B C2 A8 C0 03 5F D6',
-                    '20 00 80 52 FD 7B C2 A8 C0 03 5F D6'),
-    'vendor/lib64/libsec-ril.so': blob_fixup()
-        .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
-            '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
-    'vendor/lib64/libwvhidl.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    'vendor/lib64/libssl-tm.so': blob_fixup()
-        .replace_needed('libcrypto.so', 'libcrypto-tm.so'),
+                     '20 00 80 52 FD 7B C2 A8 C0 03 5F D6'),
+    # Audio - Effects
     'vendor/lib64/soundfx/libswdap.so': blob_fixup()
         .sig_replace('08 09 40 f9 00 01 3f d6',
-                    '20 00 80 52 1f 20 03 d5'),
+                     '20 00 80 52 1f 20 03 d5'),
+    # DRM Widevine
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
+    # libssl
+    'vendor/lib64/libssl-tm.so': blob_fixup()
+        .replace_needed('libcrypto.so', 'libcrypto-tm.so'),
+    # Keymint
+    'vendor/lib64/vendor.samsung.hardware.keymint-V1-ndk_platform.so': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so',
+                        'android.hardware.security.keymint-V4-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so',
+                        'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so',
+                        'android.hardware.security.sharedsecret-V1-ndk.so')
+        .replace_needed('libcrypto.so', 'libcrypto-tm.so')
+        .replace_needed('libssl.so', 'libssl-tm.so')
+        .add_needed('android.hardware.security.rkp-V3-ndk.so')
+        .add_needed('libshim_crypto.so'),
+    'vendor/etc/init/android.hardware.security.keymint-service.samsung.rc': blob_fixup()
+        .regex_replace('android\\.hardware\\.security\\.keymint-service\n',
+                       'android.hardware.security.keymint-service.samsung\n'),
+    # RIL
+    'vendor/lib64/libsec-ril.so': blob_fixup()
+        .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
+                     '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 03 00 80 D2'),
     'vendor/lib64/libVendorSemTelephonyProps.so': blob_fixup()
         .binary_regex_replace(rb'persist\.ril\.supportNrModefromCp', b'vendor.ril.supportNrModefromCp\x00'),
     'vendor/lib64/libvkmanager_vendor.so': blob_fixup()
         .binary_regex_replace(rb'ro\.factory\.factory_binary', b'ro.vendor.factory_binary\x00'),
+    # Sensors
+    'vendor/lib64/libsensorlistener.so': blob_fixup()
+        .add_needed('libshim_sensorndkbridge.so'),
+    (
+        'vendor/lib64/sensors.grip.so',
+        'vendor/lib64/sensors.inputvirtual.so',
+        'vendor/lib64/sensors.sensorhub.so',
+    ): blob_fixup()
+        .add_needed('libutils-v32.so')
+        .binary_regex_replace(b'_ZN7android6Thread3runEPKcim', b'_ZN7utils326Thread3runEPKcim')
+        .remove_needed('libhidltransport.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
