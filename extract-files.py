3f95df055e03af0c9d14686d53c7be1cb71d929f
Majaahh <maja.blaszczak@proton.me>
2025-10-24 00:26:25 +0200

    s5e8825-common: Commonize keymint blobs
    Change-Id: I5788612a61ef3b9447ba89882651d00976d16341
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -66,10 +66,33 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libssl-tm.so': blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-tm.so'),
     # Keymint
-    'vendor/lib64/vendor.samsung.hardware.keymint-V1-ndk_platform.so': blob_fixup()
+    (
+        'vendor/lib64/libskeymint10device.so',
+        'vendor/lib64/libskeymint_cli.so',
+        'vendor/lib64/vendor.samsung.hardware.keymint-V1-ndk_platform.so',
+    ): blob_fixup()
         .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so',
                         'android.hardware.security.keymint-V4-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so',
+                        'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so',
+                        'android.hardware.security.sharedsecret-V1-ndk.so')
         .add_needed('android.hardware.security.rkp-V3-ndk.so')
+        .add_needed('libshim_crypto.so')
+        .replace_needed('libcrypto.so', 'libcrypto-tm.so'),
+    'vendor/bin/hw/android.hardware.security.keymint-service.samsung': blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so',
+                        'android.hardware.security.keymint-V3-ndk.so')
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform',
+                        'android.hardware.security.keymint-V3-ndk')
+        .replace_needed('android.hardware.security.keymint-V1-ndk',
+                        'android.hardware.security.keymint-V3-ndk')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so',
+                        'android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so',
+                        'android.hardware.security.sharedsecret-V1-ndk.so')
+        .add_needed('android.hardware.security.rkp-V3-ndk.so')
+        .replace_needed('libcrypto.so', 'libcrypto-tm.so')
         .add_needed('libshim_crypto.so'),
     # RIL
     'vendor/lib64/libsec-ril.so': blob_fixup()
