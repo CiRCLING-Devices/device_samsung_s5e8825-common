05cda40c9934cd736c8a7c69c6e923ab9a4f1d5d
Majaahh <maja.blaszczak@proton.me>
2025-11-17 20:50:57 +0100

    s5e8825-common: Disable Legacy MFC
    * Due to kernel not having required IOCTL, H264/HEVC decoding/encoding did
      not work. Legacy MFC was guarding "Get_RealTimePriority" code that is
      broken without that IOCTL. "Get_RealTimePriority" is not present in
      proprietary C2 stack but we got it working. Thanks samsung...
    
    Change-Id: Ie27ee38d6fa4e7358f61b61821e07ae234c8909c
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- common.mk
+++ common.mk
@@ -104,8 +104,6 @@ PRODUCT_PACKAGES += \
     codec2.vendor.base.policy \
     codec2.vendor.ext.policy
 
-$(call soong_config_set,openmax,legacy_mfc,true)
-
 # ConfigStore
 PRODUCT_PACKAGES += disable_configstore
 
