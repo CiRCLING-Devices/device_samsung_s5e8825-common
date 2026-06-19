amruio/android_device_samsung_s5e8825-common

s5e8825-common: Disable 10-bit C2 support
* Our MFC does not support 10-bit decoding/encoding.
Not disabling this option causes C2 codecs to falsely report 10-bit capabilities,
which can lead some players to error out.

common.mk
@@ -104,6 +104,8 @@ PRODUCT_PACKAGES += \
     codec2.vendor.base.policy \
     codec2.vendor.ext.policy
 
+$(call soong_config_set,openmax,UNSUPPORT_10BIT,true)
+
 # ConfigStore
 PRODUCT_PACKAGES += disable_configstore
 

CiRCLING-Devices/device_samsung_s5e8825-common

common.mk
@@ -106,6 +106,8 @@ PRODUCT_PACKAGES += \
     codec2.vendor.base.policy \
     codec2.vendor.ext.policy
 
+$(call soong_config_set_bool,openmax,UNSUPPORT_10BIT,true)
+
 # ConfigStore
 PRODUCT_PACKAGES += disable_configstore
 