8357c7ed2f9612dc43f3161c293e9f78e3b3bbca
Bahar KURT <kurtbahartr@users.noreply.github.com>
2026-06-28 16:51:19 +0300

    s5e8825-common: Improve the way marketname is set

--- configs/init/libinit/libinit_device.cpp
+++ configs/init/libinit/libinit_device.cpp
@@ -26,6 +26,7 @@ void search_device(const std::vector<device_info_t> devices) {
 
 void set_device_props(const device_info_t device) {
   property_override("bluetooth.device.default_name", device.full_name);
+  property_override("ro.product.marketname", device.full_name);
 
   set_ro_build_prop("device", device.codename, true);
 
